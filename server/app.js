98326e0c8f7de7609f3e1c0173b5865dfab92acf
Jhass Theisen <jhasstheisen1@gmail.com>
2022-12-01 21:20:11 -0800

    finished phase 2

--- server/app.js
+++ server/app.js
@@ -4,9 +4,36 @@ const path = require('path');
 
 
 // phase 1
+
+// use static for assets
 app.use('/static', express.static(path.join(__dirname, 'assets')));
+// middleware to use .json for all json bodies
 app.use(express.json());
+// require async errors
 require('express-async-errors');
+
+// phase 2
+
+// middleware function
+const logger = (req, res, next) => {
+  // logs url and method
+  console.log(`${req.method} ${req.url}`);
+  // want to show status, but runs before res is made
+  // res.on is event listener, when res finished
+  // print status code
+  res.on('finish', () => {
+    console.group();
+    console.log(`Status Code - ${res.statusCode}`);
+    console.groupEnd();
+  });
+  // goes next so not hanging
+  next();
+};
+// middleware to use logger
+app.use(logger);
+
+
+
 
 // For testing purposes, GET /
 app.get('/', (req, res) => {
@@ -26,7 +53,13 @@ app.get('/test-error', async (req, res) => {
   throw new Error("Hello World!")
 });
 
-
+// phase 2
+// if nothing else sends a res, couldn't be found error
+app.use((req, res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.statusCode = 404;
+  throw err;
+});
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
